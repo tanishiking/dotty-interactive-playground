50ea5babab3d6d124b98597873af371765b97bf3
Rikito Taniguchi <rikiriki1238@gmail.com>
2022-05-26 19:10:44 +0900

    update

--- build.sbt
+++ build.sbt
@@ -1,7 +1,8 @@
-val dottyVersion = "3.1.1"
+val dottyVersion = "3.1.3-RC4"
 
 lazy val root = project
   .in(file("."))
+  .enablePlugins(BuildInfoPlugin)
   .settings(
     name := "dotty-interactive-playground",
     version := "0.1.0",
@@ -12,6 +13,7 @@ lazy val root = project
       "org.scala-lang" % "scala3-compiler_3" % scalaVersion.value,
       // "org.scala-lang" %% "scala3-library" % scalaVersion.value,
       "io.get-coursier" % "interface" % "1.0.7",
-      // "com.lihaoyi" %% "pprint" % "0.7.3",
-    )
+    ),
+    buildInfoKeys := Seq[BuildInfoKey](scalaVersion),
+    buildInfoPackage := "playground"
   )
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,3 +1,5 @@
+package playground
+
 import dotty.tools.dotc.interactive.{InteractiveDriver, Interactive, Completion}
 import dotty.tools.dotc.util.{Spans, SourcePosition}
 import dotty.tools.dotc.core.Contexts._
@@ -7,7 +9,7 @@ import coursierapi.{Fetch, Dependency}
 import java.nio.file.Path
 import java.net.URI
 
-object Main {
+object Main:
 
   def main(args: Array[String]): Unit = {
     val driver = newDriver
@@ -19,11 +21,9 @@ object Main {
       // [dotty/InteractiveCompiler.scala](https://github.com/lampepfl/dotty/blob/8059fcec9135e5672cc8a0359329258c6aaf837b/compiler/src/dotty/tools/dotc/interactive/InteractiveCompiler.scala)
       // and add the result to compilation unit.
       driver.run(uri, "object X { }")
-
-      // pprint.log(driver.openedFiles)
-      // LinkedHashMap(file:///virtual -> /virtual)
     }
 
+    /*
     // ===============================================
     // Feeding partial code and run completion
     // ===============================================
@@ -31,24 +31,6 @@ object Main {
       val sourceParital = "object X { def x = 1.toSt } "
       val uriPartial = new URI("file:///partial")
       val diag = driver.run(uriPartial, sourceParital)
-      // pprint.log(diag)
-      // List(
-      //   class dotty.tools.dotc.reporting.Diagnostic$Error at /virtual:[19..21..25]: value toSt is not a member of Int - did you mean (1 : Int).toInt?,
-      //   class dotty.tools.dotc.reporting.Diagnostic$Info at ?: 1 error found
-      // )
-
-      // pprint.log(driver.openedTrees(uriPartial))
-      // Mode.Interactive makes parser error resillient using <error> symbol?
-      // ...
-      // preBody = List(
-      //       DefDef(
-      //         name = x,
-      //         tparams = List(),
-      //         vparamss = List(),
-      //         tpt = TypeTree[dotty.tools.dotc.core.Types$PreviousErrorType@752771a8],
-      //         preRhs = Select(qualifier = Literal(const = ( = 1)), name = toSt)
-      //       )
-      //     )
 
       val pos = new SourcePosition(
         driver.openedFiles(uriPartial),
@@ -56,79 +38,45 @@ object Main {
       )
       val completions = Completion.completions(pos)(using driver.currentCtx.fresh.setCompilationUnit(driver.compilationUnits.get(uriPartial).get))
       println(completions)
-      // (
-      //   21,
-      //   List(
-      //     Completion(label = "toShort", description = "=> Short", symbols = List(method toShort)),
-      //     Completion(label = "toString", description = "(): String", symbols = List(method toString))
-      //   )
-      // )
     }
 
+    */
 
-    /*
-    // ===============================================
-    // Find definition
-    // ===============================================
     locally {
-      val sourceDefinition = "object Definition { def x = 1; val y = x + 1 }"
+      val sourceDefinition = "object Definition { def x    }"
       val uriDefinition = new URI("file:///def")
       driver.run(uriDefinition, sourceDefinition)
-      given ctx as Context = driver.currentCtx
+      given ctx: Context = driver.currentCtx
 
-      val pos = new SourcePosition(driver.openedFiles(uriDefinition), Spans.Span(sourceDefinition.indexOf("x + 1")))
+      val pos = new SourcePosition(driver.openedFiles(uriDefinition), Spans.Span(sourceDefinition.indexOf("def x") + "def x".length))
       val path = Interactive.pathTo(driver.openedTrees(uriDefinition), pos)
+      path.foreach(println)
 
-      // Feeding path to the pos, and return definition's tree
-      val definitions = Interactive.findDefinitions(path, pos, driver)
-      // pprint.log(definitions)
-      // Oops, it founds the definition of x from another compilation unit
-      //
-      // List(
-      //   SourceTree(
-      //     tree = DefDef(
-      //       name = x,
-      //       tparams = List(),
-      //       vparamss = List(),
-      //       tpt = TypeTree[dotty.tools.dotc.core.Types$PreviousErrorType@6b6b68d0],
-      //       preRhs = Select(qualifier = Literal(const = ( = 1)), name = toSt)
-      //     ),
-      //     source = /partial
-      //   ),
-      //   SourceTree(
-      //     tree = DefDef(
-      //       name = x,
-      //       tparams = List(),
-      //       vparamss = List(),
-      //       tpt = TypeTree[TypeRef(ThisType(TypeRef(NoPrefix,module class scala)),class Int)],
-      //       preRhs = Literal(const = ( = 1))
-      //     ),
-      //     source = /def
-      //   )
-      // )
+      // 3.1.3-RC2 or later
+      // Literal(Constant(null))
+      // DefDef(x,List(),TypeTree[TypeRef(ThisType(TypeRef(NoPrefix,module class scala)),class Null)],Literal(Constant(null)))
+      // Template(DefDef(<init>,List(List()),TypeTree[TypeRef(ThisType(TypeRef(NoPrefix,module class scala)),class Unit)],EmptyTree),List(Apply(Select(New(TypeTree[TypeRef(ThisType(TypeRef(NoPrefix,module class lang)),class Object)]),<init>),List())),ValDef(_,SingletonTypeTree(Ident(Definition)),EmptyTree),List(DefDef(x,List(),TypeTree[TypeRef(ThisType(TypeRef(NoPrefix,module class scala)),class Null)],Literal(Constant(null)))))
+      // TypeDef(Definition$,Template(DefDef(<init>,List(List()),TypeTree[TypeRef(ThisType(TypeRef(NoPrefix,module class scala)),class Unit)],EmptyTree),List(Apply(Select(New(TypeTree[TypeRef(ThisType(TypeRef(NoPrefix,module class lang)),class Object)]),<init>),List())),ValDef(_,SingletonTypeTree(Ident(Definition)),EmptyTree),List(DefDef(x,List(),TypeTree[TypeRef(ThisType(TypeRef(NoPrefix,module class scala)),class Null)],Literal(Constant(null))))))
+
+      // 3.1.2
+      // DefDef(x,List(),TypeTree[TypeRef(ThisType(TypeRef(NoPrefix,module class scala)),class Null)],Literal(Constant(null)))
+      // Template(DefDef(<init>,List(List()),TypeTree[TypeRef(ThisType(TypeRef(NoPrefix,module class scala)),class Unit)],EmptyTree),List(Apply(Select(New(TypeTree[TypeRef(ThisType(TypeRef(NoPrefix,module class lang)),class Object)]),<init>),List())),ValDef(_,SingletonTypeTree(Ident(Definition)),EmptyTree),List(DefDef(x,List(),TypeTree[TypeRef(ThisType(TypeRef(NoPrefix,module class scala)),class Null)],Literal(Constant(null)))))
+      // TypeDef(Definition$,Template(DefDef(<init>,List(List()),TypeTree[TypeRef(ThisType(TypeRef(NoPrefix,module class scala)),class Unit)],EmptyTree),List(Apply(Select(New(TypeTree[TypeRef(ThisType(TypeRef(NoPrefix,module class lang)),class Object)]),<init>),List())),ValDef(_,SingletonTypeTree(Ident(Definition)),EmptyTree),List(DefDef(x,List(),TypeTree[TypeRef(ThisType(TypeRef(NoPrefix,module class scala)),class Null)],Literal(Constant(null))))))
     }
-    */
   }
 
 
-  def newDriver: InteractiveDriver = {
+  def newDriver: InteractiveDriver =
     val fetch = Fetch.create()
-
     import scala.jdk.CollectionConverters._
     fetch.addDependencies(
-      Dependency.of("org.scala-lang", "scala3-library_3", "3.1.1")
+      Dependency.of("org.scala-lang", "scala3-library_3", BuildInfo.scalaVersion)
     )
     val extraLibraries: Seq[Path] = fetch
       .fetch()
       .asScala
       .map(_.toPath())
       .toSeq
-
-    // pprint.log(extraLibraries)
-    // List(
-    //   /Users/tanishiking/Library/Caches/Coursier/v1/https/repo1.maven.org/maven2/org/scala-lang/scala3-library_3.0.0-M2/3.0.0-M2/scala3-library_3.0.0-M2-3.0.0-M2.jar,
-    //   /Users/tanishiking/Library/Caches/Coursier/v1/https/repo1.maven.org/maven2/org/scala-lang/scala-library/2.13.4/scala-library-2.13.4.jar
-    // )
 
     // As the official standard library for scala 3.0 is the Scala 2.13 library, it looks like we need to
     // add scala 2.13.4 standard library to InteractiveDriver, to manipulate scala3 code (using stdlib)
@@ -140,5 +88,5 @@ object Main {
         extraLibraries.mkString(java.io.File.pathSeparator)
       )
     )
-  }
-}
+  end newDriver
+
